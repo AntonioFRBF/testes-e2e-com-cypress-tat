de3dca910a7537e734424f11c45501766cd87535
AntonioFRBF <toniifernandez1@gmail.com>
2024-05-03 14:01:02 -0300

    Configure cypress cloud

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Login', () => {
-    it('sucessfully logs in', () => {
-        cy.guiLogin()
-        cy.contains('a', 'Create a new note').should('be.visible')
-    });
-});
+  it('sucessfully logs in', () => {
+    cy.guiLogin()
+    cy.contains('a', 'Create a new note').should('be.visible')
+  })
+})
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,94 +1,94 @@
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
-    cy.intercept('GET', '**/notes').as('getNotes')
-    cy.visit('/signup')
-    cy.get('#email').type(email)
-    cy.get('#password').type(password, { log: false })
-    cy.get('#confirmPassword').type(password, { log: false })
-    cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
-    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-      sentTo: email
-    }).then(message => {
-      const confirmationCode = message.html.body.match(/\d{6}/)[0] // expressão regular para pegar somente os digitos e pega 6 digitos. como que fez essa verificação do verification code?
-      cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
-      cy.wait('@getNotes')
-    })
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: email
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0] // expressão regular para pegar somente os digitos e pega 6 digitos. como que fez essa verificação do verification code?
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+    cy.wait('@getNotes')
   })
+})
 
 Cypress.Commands.add('guiLogin', (
-    username = Cypress.env('USER_EMAIL'),
-    password = Cypress.env('USER_PASSWORD')
-  ) => {
-    cy.intercept('GET', '**/notes').as('getNotes')
-    cy.visit('/login')
-    cy.get('#email').type(username)
-    cy.get('#password').type(password, { log: false })
-    cy.contains('button', 'Login').click()
-    cy.wait('@getNotes') // eu sei que tem de fazer essas requisições após o login
-    // mas, espera a requisição do wait acontecer, para fazer as verificações
-    // getnotes, vai buscar todas as anotações, e note vai buscar anotação em especifico
-    cy.contains('h1', 'Your Notes').should('be.visible')
-  })
-  
-  Cypress.Commands.add('sessionLogin', (
-    username = Cypress.env('USER_EMAIL'),
-    password = Cypress.env('USER_PASSWORD')
-  ) => {
-    const login = () => cy.guiLogin(username, password)
-    cy.session(username, login) 
-    // o primeiro argumento, o id da sessão, verificar se já existe uma sesão criadoa ou não 
-    // e o id, é o usuario. se já fez o login uma vez com o usuari oque está sendo passado, se já fez, não faz de novo
-    // senão fez, ele va executar a variavel login passando as variaveis de username e password
-  })
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.visit('/login')
+  cy.get('#email').type(username)
+  cy.get('#password').type(password, { log: false })
+  cy.contains('button', 'Login').click()
+  cy.wait('@getNotes') // eu sei que tem de fazer essas requisições após o login
+  // mas, espera a requisição do wait acontecer, para fazer as verificações
+  // getnotes, vai buscar todas as anotações, e note vai buscar anotação em especifico
+  cy.contains('h1', 'Your Notes').should('be.visible')
+})
 
-  const attachFileHandler = () => {
-    cy.get('#file').selectFile('cypress/fixtures/example.json')
+Cypress.Commands.add('sessionLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  const login = () => cy.guiLogin(username, password)
+  cy.session(username, login)
+  // o primeiro argumento, o id da sessão, verificar se já existe uma sesão criadoa ou não
+  // e o id, é o usuario. se já fez o login uma vez com o usuari oque está sendo passado, se já fez, não faz de novo
+  // senão fez, ele va executar a variavel login passando as variaveis de username e password
+})
+
+const attachFileHandler = () => {
+  cy.get('#file').selectFile('cypress/fixtures/example.json')
+}
+
+Cypress.Commands.add('createNote', (note, attachFile = false) => {
+  cy.visit('/notes/new')
+  cy.get('#content').type(note)
+
+  if (attachFile) {
+    attachFileHandler()
   }
-  
-  Cypress.Commands.add('createNote', (note, attachFile = false) => {
-    cy.visit('/notes/new')
-    cy.get('#content').type(note)
-  
-    if (attachFile) {
-      attachFileHandler()
-    }
-  
-    cy.contains('button', 'Create').click()
-  
-    cy.contains('.list-group-item', note).should('be.visible')
-  })
-  
-  Cypress.Commands.add('editNote', (note, newNoteValue, attachFile = false) => {
-    cy.intercept('GET', '**/notes/**').as('getNote') // quando entra na anotação que requição acontece
-  
-    cy.contains('.list-group-item', note).click()
-    cy.wait('@getNote')
-  
-    cy.get('#content')
-      .as('contentField')
-      .clear()
-    cy.get('@contentField')
-      .type(newNoteValue)
-  
-    if (attachFile) {
-      attachFileHandler()
-    }
-  
-    cy.contains('button', 'Save').click()
-  
-    cy.contains('.list-group-item', newNoteValue).should('be.visible')
-    cy.contains('.list-group-item', note).should('not.exist')
-  })
-  
-  Cypress.Commands.add('deleteNote', note => {
-    cy.contains('.list-group-item', note).click()
-    cy.contains('button', 'Delete').click()
-  
-    cy.get('.list-group-item') // verificação positiva
-        .its('length') // comprimento de pelo menos 1 elemento
-        .should('be.at.least', 1)
-    cy.contains('.list-group-item', note)
-        .should('not.exist') // sempre que fazemos uma verificação negativa, temos de fazer uma positiva antes
+
+  cy.contains('button', 'Create').click()
+
+  cy.contains('.list-group-item', note).should('be.visible')
+})
+
+Cypress.Commands.add('editNote', (note, newNoteValue, attachFile = false) => {
+  cy.intercept('GET', '**/notes/**').as('getNote') // quando entra na anotação que requição acontece
+
+  cy.contains('.list-group-item', note).click()
+  cy.wait('@getNote')
+
+  cy.get('#content')
+    .as('contentField')
+    .clear()
+  cy.get('@contentField')
+    .type(newNoteValue)
+
+  if (attachFile) {
+    attachFileHandler()
+  }
+
+  cy.contains('button', 'Save').click()
+
+  cy.contains('.list-group-item', newNoteValue).should('be.visible')
+  cy.contains('.list-group-item', note).should('not.exist')
+})
+
+Cypress.Commands.add('deleteNote', note => {
+  cy.contains('.list-group-item', note).click()
+  cy.contains('button', 'Delete').click()
+
+  cy.get('.list-group-item') // verificação positiva
+    .its('length') // comprimento de pelo menos 1 elemento
+    .should('be.at.least', 1)
+  cy.contains('.list-group-item', note)
+    .should('not.exist') // sempre que fazemos uma verificação negativa, temos de fazer uma positiva antes
 })
 
 // cypress/support/commands.js
