590a4a5208d11d8d5c447b791bffe6fffbe486f7
AntonioFRBF <toniifernandez1@gmail.com>
2024-05-03 16:48:46 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,10 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
+    defaultCommandTimeout: 15000,
     env: {
       viewportWidthBreakpoint: 768,
     },
   },
-  projectId: '37a11607-9297-451b-9113-65bb3c93eae2',
+  projectId: 'fxvoom'
 })
